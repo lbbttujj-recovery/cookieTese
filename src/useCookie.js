b520ee770dd0c4f7ececcc0b5cd13d98f338f06d
Иван Олинов <ivolinov@sberbank.ru>
2025-07-09 17:51:17 +0300

    добавить метрики в хук

--- src/useCookie.js
+++ src/useCookie.js
@@ -5,24 +5,41 @@ export const useYandexMetrika = (counterId, consentGiven) => {
     if (!consentGiven) return;
 
 // Загружаем скрипт Яндекс.Метрики
-const script = document.createElement('script');
-script.src = `https://mc.yandex.ru/metrika/tag.js`;
-script.async = true;
+const yandexMetrikaScript = document.createElement('script');
+yandexMetrikaScript.type = 'text/javascript';
+yandexMetrikaScript.text = `
+  (function(m,e,t,r,i,k,a){
+    m[i] = m[i] || function() { (m[i].a = m[i].a || []).push(arguments) };
+    m[i].l = 1 * new Date();
+    for (var j = 0; j < document.scripts.length; j++) { 
+      if (document.scripts[j].src === r) { return; }
+    }
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0];
+    k.async = 1;
+    k.src = r;
+    a.parentNode.insertBefore(k, a);
+  })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-script.onload = () => {
-  window.ym = window.ym || function(){(window.ym.q = window.ym.q || []).push(arguments)};
-  window.ym(counterId, 'init', {
+  ym(103270854, "init", {
     clickmap: true,
     trackLinks: true,
     accurateTrackBounce: true,
     webvisor: true
   });
-};
+`;
+document.head.appendChild(yandexMetrikaScript);
 
-document.head.appendChild(script);
+// 2. Добавляем <noscript> (для случаев, когда JavaScript отключен)
+const yandexMetrikaNoScript = document.createElement('noscript');
+yandexMetrikaNoScript.innerHTML = `
+  <div>
+    <img src="https://mc.yandex.ru/watch/103270854" style="position:absolute; left:-9999px;" alt="" />
+  </div>
+`;
+document.body.appendChild(yandexMetrikaNoScript);
 
 return () => {
-  document.head.removeChild(script);
+  document.head.removeChild(yandexMetrikaNoScript);
 };
   }, [counterId, consentGiven]);
 };
